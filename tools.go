7a51db86988b19660acbd2bc302cddfedab1b8d8
Ubeydu <u.keles@gmail.com>
2023-08-20 13:40:01 +0300

    Added functionality for uploading only one file

--- tools.go
+++ tools.go
@@ -40,6 +40,20 @@ type UploadedFile struct {
 	FileSize         int64
 }
 
+func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool) (*UploadedFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	files, err := t.UploadFiles(r, uploadDir, renameFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return files[0], nil
+}
+
 func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
 	renameFile := true
 	if len(rename) > 0 {
@@ -102,6 +116,7 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 					uploadedFile.NewFileName = hdr.Filename
 				}
 
+				uploadedFile.OriginalFileName = hdr.Filename
 				var outfile *os.File
 
 				defer outfile.Close()
